cc6cc64f56d8054440eec29a11f1719bf662876c
Korner03 <orikorner@gmail.com>
2018-09-24 17:41:37 +0300

    finished LAST

---
+++ ITSketch/app/src/main/java/com/postpc/user/itsketch/ImgItem.java
@@ -0,0 +1,14 @@
+package com.postpc.user.itsketch;
+
+public class ImgItem {
+
+    private String path;
+
+    public ImgItem(String path) {
+        this.path = path;
+    }
+
+    public String getPath() {
+        return path;
+    }
+}
