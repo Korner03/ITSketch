8aa91eb40a33feca6607286091d7523e281a1fbc
Korner03 <orikorner@gmail.com>
2018-09-24 13:26:31 +0300

    finished v1

--- ITSketch/app/src/main/java/com/example/user/myapplication/MainActivity.java
+++ ITSketch/app/src/main/java/com/example/user/myapplication/MainActivity.java
@@ -1,12 +1,14 @@
 package com.example.user.myapplication;
 
 import android.content.Intent;
+import android.graphics.Paint;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import java.io.File;
 import java.util.ArrayList;
@@ -32,11 +34,14 @@ public class MainActivity extends AppCompatActivity implements NewPortfolioFragm
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
         workerThread = new Thread(new ThreadWorker());
         workerThread.start();
 
-        ImageView sendCalibImg = findViewById(R.id.get_caliberator);
-        sendCalibImg.setOnClickListener(new View.OnClickListener() {
+        TextView firstTimeUser = findViewById(R.id.first_time_user);
+        firstTimeUser.setPaintFlags(firstTimeUser.getPaintFlags() |   Paint.UNDERLINE_TEXT_FLAG);
+
+        firstTimeUser.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 Log.i(TAG, "Get Caliberator Button Clicked");
 
